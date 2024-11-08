dae2cb02d44c9c937105afd8965493da885a51de
王子鑫 <zixin.wang01@lkcoffee.com>
2024-11-08 10:55:11 +0800

    feat: init

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthGuard } from './auth.guard';
+import { EmailModule } from './email/email.module';
+import { PermissionGuard } from './permission.guard';
+import { PrismaModule } from './prisma/prisma.module';
+import { RedisModule } from './redis/redis.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    UserModule,
+    EmailModule,
+    RedisModule,
+    PrismaModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: PermissionGuard,
+    },
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AllExceptionFilter } from './all-exception.filter';
+import { AppModule } from './app.module';
+import { ResponseInterceptor } from './response.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  await app.listen(3000);
+}
+bootstrap();
